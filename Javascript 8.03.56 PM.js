8e0f3dce78a61abf22f95d9dbd6f57ecd5ee83bd
Esterling Accime <accimeesterlin@yahoo.com>
2016-11-07 20:04:31 -0500

    my Portfolio

---
+++ Javascript 8.03.56 PM.js
@@ -0,0 +1,10 @@
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+
+
+function down(quantity, element){
+  $(element).css('margin-top',quantity);
+}
